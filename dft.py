16ec772bd09c5c07eabf35f15fbb7376af6646e8
TobyBoyne <tobyboyne@gmail.com>
2020-11-26 14:01:14 +0000

    Added timing to compare DFT to FFT

--- dft.py
+++ dft.py
@@ -1,5 +1,8 @@
+"""Implementation of a Discrete Fourier Transform with time comparison"""
+
 import numpy as np
 import matplotlib.pyplot as plt
+from timeit import timeit
 
 def dft(signal):
 	"""Perform a discrete fourier transform on an input signal"""
@@ -15,17 +18,24 @@ def dft(signal):
 
 if __name__ == '__main__':
 	fig, (ax_sig, ax_fourier) = plt.subplots(nrows=2)
-	N = 300
+	N = 1000
 	total_time = 20.
 	T = total_time / N
 
 	x = np.linspace(0., N*T, N)
 	xf = np.linspace(0., 1./(2.*T), N//2)
 
-	sig = np.sin(1.5 * 2.*np.pi*x)
+	sig = np.sin(1.5 * 2.*np.pi*x) + 2*np.sin(3. * 2.*np.pi*x)
 
 	yf = dft(sig)
 	yf_fast = np.fft.fft(sig)
+
+	# time test
+	n_repeats = 20
+	t_dft = timeit(lambda: dft(sig), number=n_repeats) / n_repeats
+	t_fast = timeit(lambda: np.fft.fft(sig), number=n_repeats) / n_repeats
+	print(f'DFT time taken: {t_dft * 1e3:.4f} ms')
+	print(f'FFT time taken: {t_fast * 1e3:.4f} ms')
 
 	ax_sig.plot(x, sig)
 	ax_fourier.plot(xf, 2./N * np.abs(yf_fast[0:N//2]))
