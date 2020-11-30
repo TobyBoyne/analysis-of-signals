72f138d2e3aa09a78abbd5d5fb1fbbc1b62c0d97
TobyBoyne <tobyboyne@gmail.com>
2020-11-30 17:43:18 +0000

    Plotted varying sample times with a changing frequency

--- sampling/sample_time_plot.py
+++ sampling/sample_time_plot.py
@@ -11,10 +11,20 @@ def get_scaling_window(window_size, t_size):
 		return w
 	return window
 
+def sharp_signal(ts):
+	def signal():
+		N = len(ts)
+		frequencies = np.ones_like(ts)
+		frequencies[:N // 3] = 	8.
+		frequencies[N // 3:] =	1.
+		# frequencies[2*N // 3:] =		0.5
+		out = np.sin(2 * np.pi * frequencies * ts)
+		return out
 
+	return signal
 
 class SampleTimeAnimator(Animator):
-	def __init__(self, axes, signal_func, ts, fs, window_func, window_size, **kwargs):
+	def __init__(self, axes, signal_func, ts, fs, window_func, window_size, total_frames, **kwargs):
 		super().__init__(axes, None, signal_func, ts, fs, **kwargs)
 
 		self.signal = signal_func()
@@ -23,7 +33,7 @@ class SampleTimeAnimator(Animator):
 		self.ax_time.set_ylim(-1, 1)
 
 		self.ax_freq.set_xlim(0, 10)
-		self.ax_freq.set_ylim(0, 1.5)
+		self.ax_freq.set_ylim(1e-3, 1.5)
 
 		self.line_time.set_linestyle('--')
 		self.line_time.set_alpha(0.5)
@@ -33,15 +43,17 @@ class SampleTimeAnimator(Animator):
 		self.window_func = window_func
 		self.window_line, = self.ax_time.plot(ts, window_func(0))
 
-		wavelengths = fr'$=\frac{{{window_size//50}}}{{2}}$'
-		self.ax_time.set_title(f'Window size{wavelengths} wavelengths')
+		wavelengths = fr'$=\frac{{{window_size//50}}}{{2}}$' + ' wavelengths'
+		wavelengths = f' = {window_size} samples'
+		self.ax_time.set_title(f'Window size{wavelengths}')
 
 		self.window_size = window_size
 		# find the number of times each cycle needs to be repeated
 		self.repeat_samples = int(np.ceil(len(ts) / window_size))
 
+		self.frames = total_frames
 		# plot the real frequency
-		self.real_freq_line = self.ax_freq.vlines(1, 1e-5, 1, color='tab:green', lw=3, linestyle='--')
+		self.real_freq_line = self.ax_freq.vlines([1, 8], 1e-5, 1, color='tab:green', lw=3, linestyle='--')
 
 	def repeat_sample(self, wave, offset):
 		"""For a sample of the wave, repeat it for the entire range of ts"""
@@ -52,7 +64,7 @@ class SampleTimeAnimator(Animator):
 
 	def evaluate(self, i):
 		"""Signal is to be evaluated for ts"""
-		k = i * 3
+		k = int((i / self.frames) * (len(ts) - self.window_size))
 
 		window = self.window_func(k)
 
@@ -76,24 +88,28 @@ if __name__ == '__main__':
 	total_time = 10.
 	T = total_time / N
 
+	ANIM_TIME = 5
+	total_frames = int((ANIM_TIME * 1000) // INTERVAL)
+
 	ts = np.linspace(0., total_time, N)
 	fs = np.linspace(0., 1. / (2. * T), N // 2)
 
 	fig, all_axes = plt.subplots(nrows=2, ncols=3, sharey='row', figsize=(15, 10))
 	signal = lambda: np.sin(2 * np.pi * ts)
+	signal = sharp_signal(ts)
 	window_sizes = (50, 350, 650)
 
 	anims = []
 	for ax_pair, window_size in zip(all_axes.T, window_sizes):
 		window_func = get_scaling_window(window_size, len(ts))
-		anim = SampleTimeAnimator(ax_pair, signal, ts, fs, window_func, window_size,
-								  log_amplitude=False)
+		anim = SampleTimeAnimator(ax_pair, signal, ts, fs, window_func, window_size, total_frames,
+								  log_amplitude=True)
 		anims.append(anim)
 
 	handles = (anim.line_freq, anim.window_line, anim.real_freq_line)
-	fig.legend(handles, ('Signal', 'Sampled region', 'Real frequency'))
+	fig.legend(handles, ('Signal', 'Sampled region', 'Real frequency values'))
 
-	grp = GroupAnimator(fig, anims, 3)
-	grp.save('sample_time_sine.gif', writer='ffmpeg')
+	grp = GroupAnimator(fig, anims, ANIM_TIME)
+	grp.save('sample_time_changefrequency.gif', writer='ffmpeg')
 	plt.show()
 
