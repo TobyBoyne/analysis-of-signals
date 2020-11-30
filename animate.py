9c87fa8a3d0ceba3b42fc93e54ce060244bef99c
TobyBoyne <tobyboyne@gmail.com>
2020-11-30 12:58:16 +0000

    Expanded plotting of window functions

--- animate.py
+++ animate.py
@@ -41,19 +41,18 @@ class Animator:
 	def __init__(self, axes, window_func, signal_func, ts, fs, **kwargs):
 		self.ax_time, self.ax_freq = axes
 
-		self.ax_time.set_xlim(0, np.max(ts))
-		self.ax_time.set_ylim(-1, 1)
-		self.ax_time.set_xlabel('time [s]')
 
-		self.ax_freq.set_xlim(0, 2)
-		self.ax_freq.set_ylim(0, 1.5)
+		self.ax_time.set_xlabel('time [s]')
 		self.ax_freq.set_xlabel('frequency [Hz]')
 
 		self.window = window_func(ts) if window_func is not None else rectangular(ts)
 		self.signal = signal_func
 
 		self.line_time, = self.ax_time.plot(ts, np.zeros_like(ts), lw=3)
-		self.line_freq, = self.ax_freq.plot(fs, np.zeros_like(fs), lw=3)
+		if kwargs.get('log_amplitude', False):
+			self.line_freq, = self.ax_freq.semilogy(fs, np.zeros_like(fs), lw=3)
+		else:
+			self.line_freq, = self.ax_freq.plot(fs, np.zeros_like(fs), lw=3)
 
 		self.ts = ts
 		self.fs = fs
@@ -61,8 +60,8 @@ class Animator:
 		self.y_freq = []
 
 		# plot window function on time axis
-		if kwargs.get('plot_window', False):
-			self.ax_time.plot(ts, self.window)
+		# if kwargs.get('plot_window', False):
+
 
 	def evaluate(self, k):
 		"""Base animator class has no evalution method"""
@@ -76,30 +75,38 @@ class Animator:
 		return self.evaluate(i)
 
 
-class WindowFuncAnimator(Animator):
-	def __init__(self, axes, window_func, signal_func, ts, fs, **kwargs):
-		super().__init__(axes, window_func, signal_func, ts, fs, **kwargs)
+
+class SampleRateAnimator(Animator):
+	def __init__(self, axes, signal_func, t_max, fs, **kwargs):
+		ts = np.linspace(0, t_max, 1)
+		super().__init__(axes, None, signal_func, ts, fs, **kwargs)
+		self.t_max = t_max
+
+		self.ax_time.set_xlim(0, t_max)
+		self.ax_time.set_ylim(-1, 1)
+
+		self.ax_freq.set_xlim(0, 25)
+		self.ax_freq.set_ylim(0, 1.5)
+
 
 	def evaluate(self, i):
-		k = (i + 10) * (INTERVAL / 1000) / 4
+		"""Signal is to be evaluated for ts"""
+		k = (i + 10) // 2
+		self.ts = np.linspace(0, self.t_max, int(k))
 		N = len(self.ts)
+		T = self.t_max / N
+		self.fs = np.linspace(0., 1. / (2. * T), N // 2)
 
-		self.y_time = self.signal(k) * self.window
-		self.line_time.set_ydata(self.y_time)
+
+		self.y_time = self.signal(self.ts)
+		self.line_time.set_data(self.ts, self.y_time)
 
 		fft = np.fft.fft(self.y_time)
 		self.y_freq = 2. / N * np.abs(fft[0:N // 2])
-		self.line_freq.set_ydata(self.y_freq)
+		self.line_freq.set_data(self.fs, self.y_freq)
 
 		return [self.line_time, self.line_freq]
 
-
-class SampleRateAnimator(Animator):
-	def __init__(self, axes, signal_func, ts, fs, **kwargs):
-		super().__init__(axes, None, signal_func, ts, fs, **kwargs)
-
-	def evaluate(self, k):
-		pass
 
 
 if __name__ == '__main__':
@@ -117,6 +124,11 @@ if __name__ == '__main__':
 	window_func = flat_top
 
 	signal = lambda k: np.sin(2*np.pi*k*ts / 4)
-	anim = Animator(axes, window_func, signal, ts, fs)
+	# anim = Animator(axes, window_func, signal, ts, fs)
+	# group_anim = GroupAnimator(fig, [anim], 3)
+
+	signal = lambda t: np.sin(2*np.pi*t*2)
+	anim = SampleRateAnimator(axes, signal, 2, fs)
 	group_anim = GroupAnimator(fig, [anim], 3)
+
 	plt.show()
--- window/window_plot.py
+++ window/window_plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from window.window_funcs import *
-from animate import GroupAnimator, WindowFuncAnimator
+from animate import GroupAnimator, Animator, INTERVAL
 import read_data
 
 # signal functions
@@ -13,6 +13,34 @@ def sine_sweep(ts):
 def real_signal(data):
 	"""Return a function that gives a sample of a real signal"""
 	pass
+
+
+class WindowFuncAnimator(Animator):
+	def __init__(self, axes, window_func, signal_func, ts, fs, **kwargs):
+		super().__init__(axes, window_func, signal_func, ts, fs, **kwargs)
+		self.ax_time.plot(ts, self.window)
+
+		self.ax_time.set_xlim(0, np.max(ts))
+		self.ax_time.set_ylim(-1, 1)
+
+		self.ax_freq.set_xlim(0, 2)
+		if kwargs.get('log_amplitude', False):
+			self.ax_freq.set_ylim(1e-4, 2e0)
+		else:
+			self.ax_freq.set_ylim(0, 1.5)
+
+	def evaluate(self, i):
+		k = (i + 10) * (INTERVAL / 1000) / 4
+		N = len(self.ts)
+
+		self.y_time = self.signal(k) * self.window
+		self.line_time.set_ydata(self.y_time)
+
+		fft = np.fft.fft(self.y_time)
+		self.y_freq = 2. / N * np.abs(fft[0:N // 2])
+		self.line_freq.set_ydata(self.y_freq)
+
+		return [self.line_time, self.line_freq]
 
 if __name__ == '__main__':
 	N = 1000
@@ -33,10 +61,10 @@ if __name__ == '__main__':
 	anims = []
 	for ax_pair, window_func in zip(all_axes.T, windows):
 		anim = WindowFuncAnimator(ax_pair, window_func, signal, ts, fs,
-						plot_window=True)
+						plot_window=True, log_amplitude=True)
 		anims.append(anim)
 
 	grp = GroupAnimator(fig, anims, 5)
 	fig.legend(('Signal', 'Window Function'))
-	# grp.save('window_sine_sweep.gif', 'ffmpeg')
+	grp.save('window_log_sweep.gif', 'ffmpeg')
 	plt.show()
